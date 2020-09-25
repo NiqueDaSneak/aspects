7323c182beed6020baf3a4ab6fbf846261d094b5
niquedasneak <thisisdom4@gmail.com>
2020-09-25 17:38:05 -0400

    remove context in lieu of hooks state

--- App.js
+++ App.js
@@ -1,35 +1,15 @@
-import React, { useState, useRef } from 'react'
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  TouchableOpacity, 
-  Animated, 
-  Switch,
-  TextInput,
-} from 'react-native'
-import * as Animatable from 'react-native-animatable'
+import React from 'react'
 import Layout from './src/assets/Layout'
 import Hub from './src/pages/Hub'
-import { ModalContext } from './src/state/modal.context'
-import { useModal } from './src/hooks/modal.hook'
-
 
 const App = () => {
-
-  const modal = useModal()
-
   return (
-    <ModalContext.Provider value={modal}>
+    <>
       <Layout>
         <Hub />
-
       </Layout>
-    </ModalContext.Provider>
+    </>
   )
 }
-
-// const styles = StyleSheet.create({
-// })
 
 export default App
