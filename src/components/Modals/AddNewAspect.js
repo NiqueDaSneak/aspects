0bfa04058c7fa509ff4328dafb73b9c83b707627
niquedasneak <thisisdom4@gmail.com>
2020-10-19 20:18:26 -0400

    add new modals

--- src/components/Modals/AddNewAspect.js
+++ src/components/Modals/AddNewAspect.js
@@ -9,38 +9,59 @@ import {
   Button,
   KeyboardAvoidingView,
   FlatList,
+  Modal,
+  Alert,
+  Keyboard,
+  Animated,
+  Easing
 } from 'react-native'
-import { theme } from '../../assets/utils'
-import Consideration from '../Consideration'   
+import { BlurView } from 'expo-blur'
+
+import { theme, useKeyboard } from '../../assets/utils'
 import { AspectsContext, ModalContext } from '../../state'
 
-
-const AddNewAspect = () => {
-
-  const [aspectsState, aspectsDispatch] = useContext(AspectsContext)
+const AddNewAspect = ({ visible }) => {
   const [modalState, modalDispatch] = useContext(ModalContext)
-
+  const [aspectsState, aspectsDispatch] = useContext(AspectsContext)
+  const [keyboardHeight] = useKeyboard()
+  console.log('keyboardHeight: ', keyboardHeight)
   const [aspectTitle, setAspectTitle] = useState('Useless Placeholder')
-  const [aspectType, setAspectType] = useState(false)
-  const [importance, setImportance] = useState('')
-  const [considerationCreatorInputValue, setConsiderationCreatorInputValue] = useState('')
-  const [considerations, setConsiderations] = useState([])
-
+  const [keyboardSpace, setkeyboardSpace] = useState(0)
   const inputRef = useRef()
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const slideLeft = useRef(new Animated.Value(0)).current
+  const slideLeft2 = useRef(new Animated.Value(400)).current
+  const [importance, setImportance] = useState('')
 
-  const toggleSwitch = () => setAspectType(previousState => !previousState)
+  useEffect(() => {
+    if (modalState.modalActive) {
+      inputRef.current.focus()
+    }
+  }, [modalState.modalActive])
 
-  const addNewConsideration = () => {
-    setConsiderations(considerations => [...considerations, considerationCreatorInputValue])
-    setConsiderationCreatorInputValue('')
-  }
-
+  useEffect(() => {
+    if (questionIndex > 0) {
+      Animated.timing(slideLeft, {
+        toValue: -400,
+        duration: 300,
+        useNativeDriver: false,
+        easing: Easing.ease,
+      }).start()
+      
+      Animated.timing(slideLeft2, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+        easing: Easing.ease,
+      }).start()
+    }
+  }, [questionIndex])
+  
   const submitNewAspect = () => {
     let newAspect = {
       title: aspectTitle,
-      type: aspectType ? 'long' : 'short',
       importanceStatement: importance,
-      considerations: considerations
+      considerations: []
     }
     aspectsDispatch({
       type: 'ADD_NEW_ASPECT',
@@ -49,172 +70,106 @@ const AddNewAspect = () => {
     modalDispatch({
       type: 'CLOSE_MODAL'
     })
-    // closeModal()
   }
 
-  useEffect(() => {
-    if (modalState.modalActive) {
-      inputRef.current.focus()
-    }
-  }, [modalState.modalActive])
-
-
   return(
-    <KeyboardAvoidingView 
-      behavior="padding"
-      style={styles.newAspectModal}
+    <Modal
+      animationType='slide'
+      transparent={true}
+      visible={visible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.')
+      }}
     >
-      <Text style={{
-        textAlign: 'center',
-        fontSize: theme.fonts.sizes.large 
-      }}>Add New Aspect</Text>
-      <Text style={{
-        fontSize: theme.fonts.sizes.small 
-      }}>Give Your Aspect A Title</Text>
-      <TextInput
-        ref={inputRef}
-        multiline={true}
-        blurOnSubmit
-        maxLength={41}
-        keyboardAppearance={'dark'}
-        returnKeyType={'done'}          
-        style={styles.aspectTitleInput}
-        onChangeText={text => setAspectTitle(text)}
-        placeholder="Useless Placeholder"
-      /> 
-      <Text style={{
-        fontSize: theme.fonts.sizes.small 
-      }}>Is this a short term or long term consideration?</Text>
-      <View style={styles.swtichContainer}>
-        <Text style={{
-          fontSize: theme.fonts.sizes.small 
-        }}>Short Term</Text>
-        <Switch
-          trackColor={{
-            false: '#767577',
-            true: '#81b0ff' 
-          }}
-          thumbColor={aspectType ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={aspectType}
-        />
-        <Text style={{
-          fontSize: theme.fonts.sizes.small 
-        }}>Long Term</Text>
-      </View>
-      <Text style={{
-        fontSize: theme.fonts.sizes.small 
-      }}>Why is this important to you?</Text>
-      <TextInput
-        keyboardAppearance={'dark'}
-        blurOnSubmit
-        returnKeyType={'done'}         
-        multiline={true}
-        numberOfLines={4}
-        style={styles.importanceInput}
-        onChangeText={text => setImportance(text)}
-      />
-      <Text style={{
-        fontSize: theme.fonts.sizes.small 
-      }}>What tasks will address this?</Text>
-      <View style={styles.newConsiderationContainer}>
-        <TextInput
-          multiline={true}
-          blurOnSubmit={true}
-          keyboardAppearance={'dark'}
-          returnKeyType={'done'}                             
-          maxLength={54}
-          style={styles.considerationCreatorInput}
-          onChangeText={(text) => {
-            setConsiderationCreatorInputValue(text)
-          }}   
-          value={considerationCreatorInputValue}
-        />
-        <View style={styles.newConsiderationBtn}>
-          <TouchableOpacity onPress={() => addNewConsideration()}>
-            <Text style={{
-              color: 'white',
-              fontSize: theme.fonts.sizes.medium 
-            }}>+</Text>
-          </TouchableOpacity> 
-        </View>
-      </View>
-      <View style={{
-        width: '100%',
-        height: '15%' 
+      <BlurView intensity={100} style={{
+        height: '100%',
+        width: '100%' 
       }}>
-        <FlatList 
-          keyExtractor={(item, index) => `${index}`}
-          horizontal
-          data={considerations}
-          renderItem={({ item: consideration }) => (
-            <Consideration key={consideration} text={consideration} />
-          )}
-        />
-      </View>
-      <Button onPress={() => submitNewAspect()} style={styles.submitBtn} title='Submit' />
-    </KeyboardAvoidingView>
+        <Animated.View 
+          style={[styles.titleContainer, {
+            bottom: keyboardHeight + 30,
+            left: slideLeft 
+          }]
+            
+          }>
+          <Text style={{
+            fontSize: theme.fonts.sizes.large,
+            marginBottom: '4%', 
+          }}>Give Your Aspect A Title</Text>
+          <TextInput
+            ref={inputRef}
+            blurOnSubmit
+            maxLength={41}
+            keyboardAppearance={'dark'}
+            returnKeyType={'next'}          
+            style={styles.titleInput}
+            onChangeText={text => setAspectTitle(text)}
+            placeholder="Useless Placeholder"
+          /> 
+          <Button color="green" title="Next" onPress={() => setQuestionIndex(questionIndex + 1)} />
+        </Animated.View>
+        <Animated.View 
+          style={[styles.importanceContainer, {
+            bottom: keyboardHeight + 30,
+            left: slideLeft2,
+          }]}>
+          <Text style={{
+            fontSize: theme.fonts.sizes.large,
+            marginBottom: '4%', 
+          }}>Why is this important to you?</Text>
+          <TextInput
+            keyboardAppearance={'dark'}
+            blurOnSubmit
+            returnKeyType={'done'}         
+            multiline={true}
+            numberOfLines={4}
+            style={styles.importanceInput}
+            onChangeText={text => setImportance(text)}
+          />
+          <Button color="green" title="Create" onPress={() => submitNewAspect()} />
+        </Animated.View>
+      </BlurView>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-  newConsiderationContainer: {
-    ...theme.layout.flex.row,  
-    alignItems: 'center', 
-    justifyContent: 'space-between'
-  },
-  newAspectModal: {
-    paddingTop: '20%',
-    paddingBottom: '20%',
+  titleContainer: {
+    width: '100%',
+    marginTop: 'auto',
+    
     display: 'flex',
-    justifyContent: 'space-evenly',
-    height: '100%'
+    alignItems: 'center',
+    position: 'absolute'
   },
-  swtichContainer: {       
-    display: 'flex', 
-    flexDirection: 'row', 
-    justifyContent: 'space-evenly',
-    alignItems: 'center'
-  },
-  considerationCreatorInput: {         
+  titleInput: { 
     borderRadius: 10, 
-    fontSize: theme.fonts.sizes.small, 
+    fontSize: theme.fonts.sizes.medium, 
     borderColor: 'gray', 
-    borderWidth: 1, 
-    width: '80%',
+    borderWidth: 1 ,
     paddingLeft: '2%',
-    height: 30,
+    marginBottom: '4%', 
+    width: '80%',
+    textAlign: 'center',
+    padding: '2%',
+  },
+  importanceContainer: {
+    width: '100%',
+    marginTop: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+    position: 'absolute'
   },
   importanceInput: { 
     borderRadius: 10, 
     height: 80, 
+    width: '80%',
+    marginBottom: '4%', 
     fontSize: theme.fonts.sizes.small, 
     borderColor: 'gray', 
     borderWidth: 1,
-    padding: '2%',
-  },
-  aspectTitleInput: { 
-    borderRadius: 10, 
-    fontSize: theme.fonts.sizes.small, 
-    height: 30,
-    borderColor: 'gray', 
-    borderWidth: 1 ,
-    paddingLeft: '2%',
-  },
-  submitBtn: {
-    paddingBottom: '4%',
-  },
-  newConsiderationBtn: {
-    width: '15%',
-    backgroundColor: 'black',
-    height: 40,
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    marginLeft: 10,
-    borderRadius: 10, 
-  },
+    padding: '4%',
+  }
 })
 
 export default AddNewAspect
